050a664fb27206939dedb39ed1e1106efdb5c3a4
Blaine Price <blaine@ario.com>
2020-10-18 10:25:54 -0400

    Create employee, position, velocity

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,42 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         .with(FpsText);
 }
 
+fn spawn_employees(mut commands: Commands) {
+    commands
+        .spawn((
+            Employee {
+                name: "Gerald".to_string(),
+            },
+            Position(0.0, 0.0),
+            Velocity(0.0, 0.0),
+        ))
+        .spawn((
+            Employee {
+                name: "Julie".to_string(),
+            },
+            Position(244.0, 244.0),
+            Velocity(0.0, 0.0),
+        ));
+}
+
+struct GreetTimer(Timer);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, mut query: Query<(&Employee, &Position, &Velocity)>) {
+    timer.0.tick(time.delta_seconds);
+    if timer.0.finished {
+        for (employee, position, velocity) in &mut query.iter() {
+            println!("hello {}!", employee.name);
+            println!("position {},{}!", position.0, position.1);
+            println!("velocity {},{}!", velocity.0, velocity.1);
+        }
+    }
+}
+
+struct Position(f32, f32);
+struct Velocity(f32, f32);
+struct Employee {
+    name: String,
+}
+
 fn main() {
     App::build()
         .add_resource(WindowDescriptor {
@@ -52,9 +88,12 @@ fn main() {
             ..Default::default()
         })
         .add_default_plugins()
-            .add_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
+        .add_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
+        .add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup.system())
+        .add_startup_system(spawn_employees.system())
         .add_system(text_update_system.system())
+        .add_system(greet_people.system())
         .run();
 }
