1bd8962631bf6faff8fb814b93240d39c0c24cc1
Blaine Price <blaine@ario.com>
2020-12-06 15:31:09 -0500

    Add simple, non-interactive sidebar

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,11 +1,13 @@
 mod dish;
 mod employee;
 mod scene;
+mod sidebar;
 mod tasks;
 
 pub use self::{
     dish::{Dish, DishPlugin, DishType},
     employee::{Destination, Employee, EmployeePlugin},
     scene::ScenePlugin,
+    sidebar::SidebarPlugin,
     tasks::{Task, Tasks, TasksPlugin},
 };
