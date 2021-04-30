22a73bce227b8ccff3101deb97eb29097c0f6c43
Blaine Price <blaine@ario.com>
2021-04-30 16:57:54 -0400

    patron asks for food

--- src/plugins/tasks.rs
+++ src/plugins/tasks.rs
@@ -7,8 +7,11 @@ pub struct TasksPlugin;
 
 impl Plugin for TasksPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system(goto.system())
-            .add_system(goto_entity.system());
+        app.insert_resource(OrdersQueue(vec![]))
+            .add_system(goto.system())
+            .add_system(goto_entity.system())
+            .add_system(order_food.system())
+            .add_system(assign_tasks.system());
         // .add_system(goto_entity.system())
         // .add_system(give_to.system())
         // .add_system(assign_tasks.system())
@@ -21,6 +24,12 @@ impl Plugin for TasksPlugin {
     }
 }
 
+pub struct Order {
+    dish: DishType,
+    patron: Entity,
+}
+
+pub struct OrdersQueue(pub Vec<Order>);
 pub struct Tasks(pub Vec<Task>);
 
 pub struct Task {
@@ -31,6 +40,7 @@ pub struct Task {
 pub enum TaskVariants {
     GoTo(Vec3),
     GoToEntity(Entity),
+    OrderFood(DishType),
 }
 
 enum TaskStatus {
@@ -52,6 +62,12 @@ impl Task {
                 return Task {
                     status: TaskStatus::New,
                     variant: TaskVariants::GoToEntity(entity),
+                }
+            }
+            TaskVariants::OrderFood(dish) => {
+                return Task {
+                    status: TaskStatus::New,
+                    variant: TaskVariants::OrderFood(dish),
                 }
             }
         }
@@ -92,19 +108,15 @@ fn goto(
     }
 }
 
-fn goto_entity(
-    mut commands: Commands,
-    mut query: Query<(Entity, &mut Tasks)>,
-    entity_query: Query<(Entity, &Transform)>,
-) {
-    for (entity, mut tasks) in query.iter_mut() {
+fn goto_entity(mut query: Query<&mut Tasks>, entity_query: Query<(Entity, &Transform)>) {
+    for mut tasks in query.iter_mut() {
         if let Some(task) = tasks.0.first_mut() {
             if let TaskVariants::GoToEntity(target_entity) = task.variant {
                 match task.status {
                     TaskStatus::New => {
                         // Find the entity to go to
-                        for (ent, transform) in entity_query.iter() {
-                            if ent == target_entity {
+                        for (entity, transform) in entity_query.iter() {
+                            if entity == target_entity {
                                 *task = Task::new(TaskVariants::GoTo(transform.translation));
                             }
                         }
@@ -114,6 +126,40 @@ fn goto_entity(
                     }
                 }
             }
+        }
+    }
+}
+
+fn order_food(mut orders_queue: ResMut<OrdersQueue>, mut query: Query<(Entity, &mut Tasks)>) {
+    for (entity, mut tasks) in query.iter_mut() {
+        if let Some(task) = tasks.0.first_mut() {
+            if let TaskVariants::OrderFood(dish) = task.variant {
+                match task.status {
+                    TaskStatus::New => {
+                        // Ask for the food
+                        orders_queue.0.push(Order {
+                            dish,
+                            patron: entity,
+                        });
+                        task.status = TaskStatus::InProgress;
+                    }
+                    TaskStatus::InProgress => {
+                        // TODO: Do I have the food?
+                    }
+                    TaskStatus::Completed => {
+                        // TODO: Go away with the food
+                        tasks.0.pop().unwrap();
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn assign_tasks(mut orders_queue: ResMut<OrdersQueue>, mut commands: Commands, query: Query<Entity, Without<Tasks>>) {
+    if let Some(order) = orders_queue.0.first() {
+        for (entity) in query.iter() {
+
         }
     }
 }
